df9be4270dcf3c06882733baf7392f3ac95e6477
Jose Vitor <jose.vitor.sem.c@gmail.com>
2023-02-16 15:13:39 -0300

    Tabela da descritiva dos dados

--- views.r
+++ views.r
@@ -6,3 +6,11 @@ head(select(data, -c(Médicos, População, Leitos, `Renda Total`))[, 6:9]) %>%
 
 head(select(data, c(Médicos, População, Leitos, `Renda Total`))) %>%
     format_tab("Algumas observações do banco de dados disponível.", digits = 2, "latex")
+
+describe(data) %>%
+    select(-c(vars, n, trimmed, mad, range)) %>%
+    rename_all(~ c(
+        "Média", "DP", "Mediana",
+        "Min", "Max", "Skew", "Kurtosis", "EP"
+    )) %>%
+    format_tab("\\label{table:desc}Medidas descritivas das variáveis mantidas durante o estudo.", "latex", digits = 0)
