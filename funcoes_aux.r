d537512ea8bdb40d327f0d466219f4abe5e25527
Jose Vitor <jose.vitor.sem.c@gmail.com>
2023-02-17 17:08:55 -0300

    Anova com SSR

--- funcoes_aux.r
+++ funcoes_aux.r
@@ -18,3 +18,26 @@ cor_matrix_plot <- function(x) {
         ggcorrplot(show.diag = F, type = "lower", lab = T, lab_size = 7) +
         theme(axis.text.x = element_text(angle = 90)))
 }
+
+anova_reduzida <- function(anova_base) {
+    p <- nrow(anova_base) + 1
+    anova_custom <- anova_base[-(p + 1), 1:2] %>%
+        as.matrix() %>%
+        colSums() %>%
+        rbind(anova_base[p - 1, 1:2]) %>%
+        rename_all(~ c("gl", "SS")) %>%
+        mutate("MS" = SS / gl)
+
+    anova_custom <- cbind(anova_custom,
+        "$F_c$" = c(anova_custom$MS[1] / anova_custom$MS[2], NA)
+    )
+
+    anova_custom <- cbind(anova_custom,
+        "P(F > $F_c$)" = c(pf(anova_custom["$F_c$"][[1]][1], p - 1, anova_base$Df[p - 1], lower.tail = F), NA)
+    )
+
+    rownames(anova_custom) <- c("Regressão", "Resíduos")
+
+
+    return(anova_custom)
+}
