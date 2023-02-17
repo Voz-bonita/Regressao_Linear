26a009c1b0ec2757649f20b9236f561e393db219
Jose Vitor <jose.vitor.sem.c@gmail.com>
2023-02-17 11:59:08 -0300

    dados sem região em uso

--- views.r
+++ views.r
@@ -1,13 +1,13 @@
-head(select(data, -c(Médicos, População, Leitos, `Renda Total`))[, 1:5]) %>%
+head(select(data_sem_regiao, -c(Médicos, População, Leitos, `Renda Total`))[, 1:5]) %>%
+    format_tab("Algumas observações do banco de dados disponível.", digits = 2, "latex")
+
+head(select(data_sem_regiao, -c(Médicos, População, Leitos, `Renda Total`))[, 6:9]) %>%
+    format_tab("Algumas observações do banco de dados disponível.", digits = 2, "latex")
+
+head(select(data_sem_regiao, c(Médicos, População, Leitos, `Renda Total`))) %>%
     format_tab("\\label{table:head}Algumas observações do banco de dados disponível.", digits = 2, "latex")
 
-head(select(data, -c(Médicos, População, Leitos, `Renda Total`))[, 6:9]) %>%
-    format_tab("Algumas observações do banco de dados disponível.", digits = 2, "latex")
-
-head(select(data, c(Médicos, População, Leitos, `Renda Total`))) %>%
-    format_tab("Algumas observações do banco de dados disponível.", digits = 2, "latex")
-
-describe(data) %>%
+describe(data_sem_regiao) %>%
     select(-c(vars, n, trimmed, mad, range)) %>%
     rename_all(~ c(
         "Média", "DP", "Mediana",
@@ -16,8 +16,8 @@ describe(data) %>%
     format_tab("\\label{table:desc}Medidas descritivas das variáveis mantidas durante o estudo.", "latex", digits = 0)
 
 (ggplot(
-    gather(select(data, -Médicos)),
-    aes(x = value, y = rep(data$Médicos, ncol(data) - 1))
+    gather(select(data_sem_regiao, -Médicos)),
+    aes(x = value, y = rep(data_sem_regiao$Médicos, ncol(data_sem_regiao) - 1))
 ) +
     geom_point() +
     facet_wrap(~key, scales = "free_x") +
