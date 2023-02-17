417826536b4d965da003fe7c548c9b9a3a80e8dd
Jose Vitor <jose.vitor.sem.c@gmail.com>
2023-02-17 15:21:13 -0300

    Conjunto de treino utilizado

--- main.r
+++ main.r
@@ -4,6 +4,7 @@ pacman::p_load(
 )
 source("funcoes_aux.r")
 
+
 data <- read_xls("dados.xls") %>%
     rename_all(~ c(
         "id", "Cidade", "Estado", "Área da Cidade",
@@ -13,10 +14,18 @@ data <- read_xls("dados.xls") %>%
         "Desempregados", "Renda p/c", "Renda Total",
         "Região geográfica"
     )) %>%
-    select(-c(id, Cidade, Estado))
+    select(-c(id, Cidade, Estado)) %>%
+    mutate("Região geográfica" = factor(data$`Região geográfica`, levels = 1:4))
 
-data$`Região geográfica` <- factor(data$`Região geográfica`, levels = 1:4)
-data_sem_regiao <- select(data, -`Região geográfica`)
+n <- nrow(data)
+set.seed(2022)
+train_i <- sample(1:n, n/2, replace = FALSE)
+val_i <- which(!(1:n %in% train_i))
+
+train_df <- data[train_i, ]
+val_df <- data[val_i, ]
+
+data_sem_regiao <- select(train_df, -`Região geográfica`)
 
 # visualmente lineares
 lin <- c("Crimes", "Leitos", "População", "Renda Total")
--- views.r
+++ views.r
@@ -16,18 +16,18 @@ describe(data_sem_regiao) %>%
     format_tab("\\label{table:desc}Medidas descritivas das variáveis mantidas durante o estudo.", "latex", digits = 0)
 
 (ggplot(
-    gather(select(data_sem_regiao, -Médicos)),
-    aes(x = value, y = rep(data_sem_regiao$Médicos, ncol(data_sem_regiao) - 1))
+    pivot_longer(data_sem_regiao, cols = -Médicos),
+    aes(x = value, y = Médicos, ncol(data_sem_regiao) - 1)
 ) +
     geom_point() +
-    facet_wrap(~key, scales = "free_x") +
+    facet_wrap(~name, scales = "free_x") +
     theme_bw() +
     ylab("Número de médicos") +
     xlab("Valor")) %>%
     ggsave(filename = "assets/dispersao_y.png", .)
 
 (ggplot(
-    pivot_longer(data, cols = -c(`Região geográfica`, Médicos)),
+    pivot_longer(train_df, cols = -c(`Região geográfica`, Médicos)),
     aes(x = log(value), y = log(Médicos), color = `Região geográfica`)
 ) +
     geom_point() +
@@ -39,11 +39,12 @@ describe(data_sem_regiao) %>%
     ggsave(filename = "assets/dispersao_logxlogy.png", .)
 
 
-cor(data[, lin], data$Médicos) %>%
-    cbind(cor(log(data[, lin]), log(data$Médicos))) %>%
+cor(train_df[, lin], train_df$Médicos) %>%
+    cbind(cor(log(train_df[, lin]), log(train_df$Médicos))) %>%
     as.data.frame() %>%
     rename_all(~ c("r_1", "r_2")) %>%
     mutate("$Z_c$" = (atanh(r_1) - atanh(r_2)) / sqrt(2 / 437)) %>%
+    mutate("P(Z > $Z_c$)" = pnorm(`$Z_c$`, lower.tail = F))
     round(2) %>%
     mutate("P(Z > $Z_c$)" = pnorm(`$Z_c$`, lower.tail = F)) %>%
     format_tab("\\label{tab:corteste}Teste de Fisher para correlação linear simples entre o número de médicos e algumas variáveis explicativas.", format = "latex")
